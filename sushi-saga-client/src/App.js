2f76038f4fbd4cac11ece70caf0291bd0f3aae02
Andrew Jones <joneseyit@protonmail.com>
2019-01-14 16:29:31 -0500

    pre eating sushi.  got sushi scrolling

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,12 +5,29 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
+
+
 class App extends Component {
+
+  state = {
+    sushi: []
+  }
+
+  fetchSushi = () => {
+    fetch(API)
+    .then(response => response.json())
+    .then(json => this.setState({ sushi: json }))
+  }
+
+  componentDidMount(){
+    this.fetchSushi()
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer allSushi={this.state.sushi} />
         <Table />
       </div>
     );
